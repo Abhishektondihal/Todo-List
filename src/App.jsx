cb3fa4a73e13624e7c9323811cde41c50f4da588
Abhishektondihal <abhishektondihal24@gmail.com>
2023-05-14 20:39:28 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import "./style.css";
+import { NewTodoform } from "./newtodoform";
+import { Todolist } from "./Todolist";
+
+export default function App() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    if (localValue == null) return [];
+
+    return JSON.parse(localValue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo(title) {
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        {
+          id: crypto.randomUUID(),
+          title,
+          completed: false,
+        },
+      ];
+    });
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id == id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
+  return (
+    <>
+      <NewTodoform onSubmit={addTodo} />
+      <div className="border">
+        <h1 className="header">Todo list </h1>
+        <Todolist
+          todos={todos}
+          toggleTodo={toggleTodo}
+          deleteTodo={deleteTodo}
+        />
+      </div>
+    </>
+  );
+}
